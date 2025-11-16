4ed0bc742cdee5356a13b9b0f0d7e6004f7aa07f
Nikunj Saini <nikunj902782@gmail.com>
2025-11-16 15:57:46 +0530

    Connected the db using ORM prisma

---
+++ backend/prisma/client.js
@@ -0,0 +1,17 @@
+import { PrismaClient } from '@prisma/client';
+
+let prisma;
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient({
+    log: ['query'],
+  });
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient({
+      log: ['query'],
+    });
+  }
+  prisma = global.prisma;
+}
+
+export default prisma;
